57fe8e1a93cfc8afdece3fe71a61627336b3b3de
dcorrea <david94.correa@gmail.com>
2015-04-15 09:39:40 +0200

    cc4r3

---
+++ src/holamundo/hoola.java
@@ -0,0 +1,13 @@
+package holamundo;
+
+public class hoola {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hola mundo");
+		String a="Hola de nuevo";
+		//comentario
+		//version 3.0
+	}
+
+}
